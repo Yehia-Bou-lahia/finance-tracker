77d93e88b291b6e91a5278fe5f88ce4d93e055c3
Yehia Boulahia <yehia.boulahia@protonmail.com>
2026-04-22 10:19:32 +0100

    feat(model):add transactions relationship to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -16,4 +16,9 @@ class Category extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
